a8c99220851a152130a109efc2ba5fc80ac5583b
silverclips818-lang <silverclips818@gmail.com>
2026-05-10 09:56:20 +0100

    Update bot settings for name and owner

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Silver-bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Silver-bot",
+  botOwner: 'Silver', // Your name
+  ownerNumber: '2348063725454', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
